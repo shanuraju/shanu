dc4990a266b0d7dbc48e9e60813bd7c166b77ad1
shanuraju <shanmukhraju4@gmail.com>
2025-09-29 18:58:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Vite + React!</h1>
+      <p>This is a simple Vite project using React.</p>
+      <h1>Vite + React</h1>
+      <h1>kbhjhgvhbn</h1>
+    </div>
+  )
+}
+
+export default App
